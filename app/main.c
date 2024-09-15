2a180f2a6bed7c5ab4576d9440200a70898bba00
wewe129 <moon200219ou@gmail.com>
2024-09-15 17:07:59 +0100

    [any me12q]

--- app/main.c
+++ app/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main() {
-  // You can use print statements as follows for debugging, they'll be visible
-Expand 9 lines
-    fgets(input, 100, stdin);
-    int n = strlen(input);
-    input[n - 1] = '\0';
-    if (!strcmp(input, "exit 0"))
-      exit(0);
-    printf("%s: command not found\n", input);
-  }
-  return 0;
+
+    char input[100];
+    while (1) {
+        printf("$ ");
+        fflush(stdout);
+        fgets(input, 100, stdin);
+        int n = strlen(input);
+        input[n - 1] = '\0';
+        if (!strcmp(input, "exit 0"))
+            exit(0);
+        printf("%s: command not found\n", input);
+    }
+    return 0;
+}
+
