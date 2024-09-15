faa6289250b86bfc70905358fd14a04250cf41a5
wewe129 <moon200219ou@gmail.com>
2024-09-15 16:18:24 +0100

    [any mes]

--- app/main.c
+++ app/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  
-  printf("$ ");
-  fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+int main(void)
+{
+    char input_buffer[100];
+
+    printf("$ ");
+    fflush(stdout);
+
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) == NULL) {
+        return 1;
+    }
+
+    size_t input_length = strlen(input_buffer);
+    if (input_length > 0 && input_buffer[input_length - 1] == '\n') {
+        input_buffer[--input_length] = '\0';
+    }
+ 
+    printf("%s: command not found\n", input_buffer);
+    return 0;
 }
